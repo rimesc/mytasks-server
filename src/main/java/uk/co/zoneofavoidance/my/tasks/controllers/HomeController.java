d7f54244efb8d860daf945ee58dcc7603b65d03c
Chris Rimes <chris.rimes@gmail.com>
2015-12-27 16:09:13 +0000

    Add some sample data and display it in a table on the home page.

--- src/main/java/uk/co/zoneofavoidance/my/tasks/controllers/HomeController.java
+++ src/main/java/uk/co/zoneofavoidance/my/tasks/controllers/HomeController.java
@@ -2,16 +2,23 @@ package uk.co.zoneofavoidance.my.tasks.controllers;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import uk.co.zoneofavoidance.my.tasks.repositories.TaskRepository;
 
 @Controller
 @RequestMapping(path = "/")
 public class HomeController {
 
+   @Autowired
+   private TaskRepository tasks;
+
    @RequestMapping(method = GET)
-   public String get() {
-      return "home";
+   public ModelAndView get() {
+      return new ModelAndView("home", "tasks", tasks.findAll());
    }
 
 }
