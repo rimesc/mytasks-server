409d0186c482da6f1e7c381825df4402f2f2a6e1
Chris Rimes <chris.rimes@gmail.com>
2016-02-06 16:19:58 +0000

    Use messages for enums in templates.

--- src/main/java/uk/co/zoneofavoidance/my/tasks/domain/State.java
+++ src/main/java/uk/co/zoneofavoidance/my/tasks/domain/State.java
@@ -1,14 +1,7 @@
 package uk.co.zoneofavoidance.my.tasks.domain;
 
-import uk.co.zoneofavoidance.my.tasks.util.EnumUtils;
-
 public enum State {
 
    OPEN, TO_DO, IN_PROGRESS, DONE, ON_HOLD, CLOSED;
-
-   @Override
-   public String toString() {
-      return EnumUtils.toString(name());
-   }
 
 }
