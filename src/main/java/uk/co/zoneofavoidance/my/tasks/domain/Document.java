1abfea36312637515e727927739e3074e192daff
Chris Rimes <chris.rimes@gmail.com>
2016-02-07 23:24:53 +0000

    Replace list of tasks on project page with number of tasks and link to non-existent task list page.
    Add an edit button for the documentation.

--- src/main/java/uk/co/zoneofavoidance/my/tasks/domain/Document.java
+++ src/main/java/uk/co/zoneofavoidance/my/tasks/domain/Document.java
@@ -22,8 +22,16 @@ public class Document {
       this.text = text;
    }
 
+   public Long getId() {
+      return id;
+   }
+
    public String getText() {
       return text;
+   }
+
+   public void setId(final Long id) {
+      this.id = id;
    }
 
    public void setText(final String text) {
