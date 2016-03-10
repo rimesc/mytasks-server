d7cedde05d5d7c72c3d25d847915a502f278881a
Chris Rimes <chris.rimes@gmail.com>
2016-03-10 22:09:15 +0000

    Split up the security configuration to facilitate configuring the UserDetailsManager as a bean.

--- src/main/java/uk/co/zoneofavoidance/my/tasks/security/SecurityConfiguration.java
+++ src/main/java/uk/co/zoneofavoidance/my/tasks/security/SecurityConfiguration.java
@@ -5,19 +5,13 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 
 @Configuration
-@EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
    /** Admin role. */
    public static final String ADMIN_ROLE = "ROLE_ADMIN";
@@ -25,29 +19,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
    @Autowired
    private DataSource dataSource;
 
-   @Autowired
-   private UserDetailsManager userDetailsManager;
-
-   @Override
-   protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-      auth.userDetailsService(userDetailsManager).passwordEncoder(passwordEncoder());
-   }
-
    @Bean
    public UserDetailsManager userDetailsManager() {
       final JdbcUserDetailsManager userDetailsManager = new JdbcUserDetailsManager();
       userDetailsManager.setDataSource(dataSource);
       return userDetailsManager;
-   }
-
-   @Override
-   protected void configure(final HttpSecurity http) throws Exception {
-      http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout().permitAll();
-   }
-
-   @Override
-   public void configure(final WebSecurity web) throws Exception {
-      web.ignoring().antMatchers("/webjars/**", "/error/**", "/css/**", "/images/**", "/js/**", "/db/**");
    }
 
    @Bean
---
+++ src/main/java/uk/co/zoneofavoidance/my/tasks/security/SecurityConfigurer.java
@@ -0,0 +1,38 @@
+package uk.co.zoneofavoidance.my.tasks.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+
+   @Autowired
+   private UserDetailsManager userDetailsManager;
+
+   @Autowired
+   private PasswordEncoder passwordEncoder;
+
+   @Override
+   protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+      auth.userDetailsService(userDetailsManager).passwordEncoder(passwordEncoder);
+   }
+
+   @Override
+   protected void configure(final HttpSecurity http) throws Exception {
+      http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout().permitAll();
+   }
+
+   @Override
+   public void configure(final WebSecurity web) throws Exception {
+      web.ignoring().antMatchers("/webjars/**", "/error/**", "/css/**", "/images/**", "/js/**", "/db/**");
+   }
+
+}
