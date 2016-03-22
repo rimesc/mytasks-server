7998a2e39f80ecc090cf018f49771810d41673de
Chris Rimes <chris.rimes@gmail.com>
2016-03-22 23:39:21 +0000

    Angular JS implementation of the projects page.

--- src/main/resources/static/js/mytasks.js
+++ src/main/resources/static/js/mytasks.js
@@ -1,22 +1,28 @@
 angular.module('mytasks', ['ngRoute'])
-	.config(function($routeProvider) {
-	    $routeProvider.when('/', {
-	      templateUrl : 'partials/dashboard.html',
-	      controller : 'home',
-	    })
-	    .otherwise({
-        redirectTo: '/'
-	    });
-  })
-  .controller('home', function($scope, $http) {
-	  $http.get('/api/projects/').success(function(data) {
-	    $scope.projects = data.projects;
-	  });
-  })
-  .controller('navbar', function($scope, $location) { 
-    $scope.isActive = function (viewLocation) {
-    	console.log(viewLocation);
-    	console.log($location.path());
-        return viewLocation === $location.path();
-    };
-  });
+
+.config(function($routeProvider) {
+	$routeProvider
+	.when('/', {
+		templateUrl : 'partials/dashboard.html',
+		controller : 'projects'
+	})
+	.when('/projects', {
+		templateUrl : 'partials/project-list.html',
+		controller : 'projects',
+	})
+	.otherwise({
+		redirectTo: '/'
+	});
+})
+
+.controller('projects', function($scope, $http) {
+	$http.get('/api/projects/').success(function(data) {
+		$scope.projects = data.projects;
+	});
+})
+ 
+.controller('navbar', function($scope, $location) { 
+	$scope.isActive = function (viewLocation) {
+		return viewLocation === $location.path();
+	};
+});
