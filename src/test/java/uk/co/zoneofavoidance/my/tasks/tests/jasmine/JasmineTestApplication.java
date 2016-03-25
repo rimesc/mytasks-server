14544b60def8f528328205e2d1ee78d5c7d9db95
Chris Rimes <chris.rimes@gmail.com>
2016-03-25 22:21:28 +0000

    Use a profile to enable the Jasmine test mapping only when running the tests.

--- src/test/java/uk/co/zoneofavoidance/my/tasks/tests/jasmine/JasmineTestApplication.java
+++ src/test/java/uk/co/zoneofavoidance/my/tasks/tests/jasmine/JasmineTestApplication.java
@@ -2,11 +2,13 @@ package uk.co.zoneofavoidance.my.tasks.tests.jasmine;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @Controller
+@Profile("jasmine")
 public class JasmineTestApplication {
 
    @RequestMapping("/")
@@ -16,7 +18,7 @@ public class JasmineTestApplication {
 
    public static void main(final String[] args) {
       new SpringApplicationBuilder(JasmineTestApplication.class)
-         .properties("server.port=9999", "security.basic.enabled=false", "spring.profiles.active=dev")
+         .properties("server.port=9999", "security.basic.enabled=false", "spring.profiles.active=dev,jasmine")
          .run(args);
    }
 
