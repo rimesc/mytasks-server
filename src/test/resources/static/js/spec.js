0efe3acb5764b5af94aeffec9d6590d68c74caa3
Chris Rimes <chris.rimes@gmail.com>
2016-04-03 16:31:37 +0100

    Add tests for project controller.

--- src/test/resources/static/js/spec.js
+++ src/test/resources/static/js/spec.js
@@ -1,6 +1,23 @@
-describe("myTasks", function() {
+var mockModalInstance = {
+	result : {
+		then : function(onClose, onDismiss) {
+			this.onClose = onClose;
+			this.onDismiss = onDismiss;
+		}
+	},
+	close : function(data) {
+		if (this.result.onClose)
+			this.result.onClose(data);
+	},
+	dismiss : function() {
+		if (this.result.onDismiss)
+			this.result.onDismiss();
+	}
+};
+		
+describe("myTasksControllers", function() {
 
-	beforeEach(module('mytasks'));
+	beforeEach(module('myTasksControllers'));
 
 	var $httpBackend, $controller;
 	beforeEach(inject(function($injector) {
@@ -13,34 +30,14 @@ describe("myTasks", function() {
 		$httpBackend.verifyNoOutstandingRequest();
 	});
 	
-	describe("the projects controller", function() {
+	describe("projectsController", function() {
 		
 		var projects = [
-            {id : 1, name : 'My first project', description : 'This is my first sample project.',
-            	numberOfOpenTasks : 3, href : '/api/projects/1'},
-            {id : 2, name : 'My second project', description : 'This is my second sample project.',
-            	numberOfOpenTasks : 2, href : '/api/projects/2'},
-            {id : 3, name : 'My third project', description : 'This is my third sample project.',
-            	numberOfOpenTasks : 11, href : '/api/projects/3'}
+            {id : 1, name : 'My first project', description : 'This is my first sample project.', numberOfOpenTasks : 3, href : '/api/projects/1'},
+            {id : 2, name : 'My second project', description : 'This is my second sample project.', numberOfOpenTasks : 2, href : '/api/projects/2'},
+            {id : 3, name : 'My third project', description : 'This is my third sample project.', numberOfOpenTasks : 11, href : '/api/projects/3'}
     	];
 
-		var mockModalInstance = {
-			result : {
-				then : function(onClose, onDismiss) {
-					this.onClose = onClose;
-					this.onDismiss = onDismiss;
-				}
-			},
-			close : function(data) {
-				if (this.result.onClose)
-					this.result.onClose(data);
-			},
-			dismiss : function() {
-				if (this.result.onDismiss)
-					this.result.onDismiss();
-			}
-		};
-		
 		var $scope;
 
 		beforeEach(function() {
@@ -52,7 +49,7 @@ describe("myTasks", function() {
 
 		it("loads the list of projects from the REST API", function() {
 			$httpBackend.expectGET('/api/projects/').respond(200, {'projects': projects});
-			var controller = $controller('projects', {
+			var controller = $controller('projectsController', {
 				$scope : $scope
 			});
 			$httpBackend.flush();
@@ -61,12 +58,12 @@ describe("myTasks", function() {
 		
 		it("adds a new project to the list on confirm", function() {
 			$httpBackend.expectGET('/api/projects/').respond(200, {'projects': projects});
-			var controller = $controller('projects', {
+			var controller = $controller('projectsController', {
 				$scope : $scope
 			});
 			$httpBackend.flush();
 			expect($scope.projects).toEqual(projects);
-			$scope.openNewProjectModal();
+			$scope.newProject();
 			var newProject = {"id": 4, "name": "My new project", "description": "This is a new project.", "numberOfOpenTasks": 0, "href": "/api/projects/4"};
 			mockModalInstance.close(newProject);
 			expect($scope.projects).toEqual(projects.concat([newProject]));
@@ -74,19 +71,195 @@ describe("myTasks", function() {
 
 		it("leaves the project list unchanged on cancel", function() {
 			$httpBackend.expectGET('/api/projects/').respond(200, {'projects': projects});
-			var controller = $controller('projects', {
+			var controller = $controller('projectsController', {
 				$scope : $scope
 			});
 			$httpBackend.flush();
 			expect($scope.projects).toEqual(projects);
-			$scope.openNewProjectModal();
+			$scope.newProject();
 			mockModalInstance.dismiss();
 			expect($scope.projects).toEqual(projects);
 		});
 
 	});
 
-	describe("the new-project controller", function() {
+	describe("projectController", function() {
+
+		var project = {id : 1, name : 'My first project', description : 'This is my first sample project.', numberOfOpenTasks : 3, href : '/api/projects/1'};
+		var readMe = {markdown: '#My first project', html: '<h1>My first project</h1>'}
+
+		var $scope, $routeParams, $location;
+
+		beforeEach(function() {
+			$scope = {};
+			$routeParams = { id: '1' };
+			inject(function($injector) {
+				$location = $injector.get('$location');
+			});
+			inject(function($uibModal) {
+		        spyOn($uibModal, 'open').and.returnValue(mockModalInstance);
+		        spyOn($location, 'path');
+		    });
+		});
+
+		it("loads the project from the REST API", function() {
+			$httpBackend.expectGET('/api/projects/1').respond(200, project);
+			$httpBackend.expectGET('/api/projects/1/readme').respond(404, { }, { }, 'Not Found');
+			var controller = $controller('projectController', {
+				$scope : $scope,
+				$routeParams: $routeParams
+			});
+			$httpBackend.flush();
+			expect($scope.project).toEqual(project);
+			expect($scope.readMe.html).toEqual('');
+			expect($scope.error).not.toBeDefined();
+		});
+
+		it("loads the project documentation from the REST API", function() {
+			$httpBackend.expectGET('/api/projects/1').respond(200, project);
+			$httpBackend.expectGET('/api/projects/1/readme').respond(200, readMe);
+			var controller = $controller('projectController', {
+				$scope : $scope,
+				$routeParams: $routeParams
+			});
+			$httpBackend.flush();
+			expect($scope.project).toEqual(project);
+			expect($scope.readMe.html).toEqual(readMe.html);
+			expect($scope.error).not.toBeDefined();
+		});
+
+		it("is in an error state if the project is not found", function() {
+			$httpBackend.expectGET('/api/projects/1').respond(404, { }, { }, 'Not Found');
+			$httpBackend.expectGET('/api/projects/1/readme').respond(404, { }, { }, 'Not Found');
+			var controller = $controller('projectController', {
+				$scope : $scope,
+				$routeParams: $routeParams
+			});
+			$httpBackend.flush();
+			expect($scope.project).not.toBeDefined();
+			expect($scope.readMe.html).toEqual('');
+			expect($scope.error).toEqual({code: 'Not Found', detail: 'The requested project could not be found.'});
+		});
+
+		it("opens the edit project modal and updates the project on confirm", function() {
+			$httpBackend.expectGET('/api/projects/1').respond(200, project);
+			$httpBackend.expectGET('/api/projects/1/readme').respond(404, { }, { }, 'Not Found');
+			var controller = $controller('projectController', {
+				$scope : $scope,
+				$routeParams: $routeParams
+			});
+			$httpBackend.flush();
+			expect($scope.project).toEqual(project);
+			$scope.editProject();
+			var updatedProject = {"id": 4, "name": "My edited project", "description": "This is an edited project.", "numberOfOpenTasks": 0, "href": "/api/projects/4"};
+			mockModalInstance.close(updatedProject);
+			expect($scope.project).toEqual(updatedProject);
+		});
+
+		it("opens the edit project modal and leaves the project unchanged on cancel", function() {
+			$httpBackend.expectGET('/api/projects/1').respond(200, project);
+			$httpBackend.expectGET('/api/projects/1/readme').respond(404, { }, { }, 'Not Found');
+			var controller = $controller('projectController', {
+				$scope : $scope,
+				$routeParams: $routeParams
+			});
+			$httpBackend.flush();
+			expect($scope.project).toEqual(project);
+			$scope.editProject();
+			mockModalInstance.dismiss();
+			expect($scope.project).toEqual(project);
+		});
+
+		it("opens the edit documentation modal and updates the project documentation on confirm", function() {
+			$httpBackend.expectGET('/api/projects/1').respond(200, project);
+			$httpBackend.expectGET('/api/projects/1/readme').respond(200, readMe);
+			var controller = $controller('projectController', {
+				$scope : $scope,
+				$routeParams: $routeParams
+			});
+			$httpBackend.flush();
+			expect($scope.readMe.html).toEqual(readMe.html);
+			$scope.editReadMe();
+			var updatedReadMe = {markdown: '# My updated project', html: '<h1>My updated project</h1>'};
+			mockModalInstance.close(updatedReadMe);
+			expect($scope.readMe.html).toEqual(updatedReadMe.html);
+		});
+
+		it("opens the edit documentation modal and leaves the project documentation unchanged on cancel", function() {
+			$httpBackend.expectGET('/api/projects/1').respond(200, project);
+			$httpBackend.expectGET('/api/projects/1/readme').respond(200, readMe);
+			var controller = $controller('projectController', {
+				$scope : $scope,
+				$routeParams: $routeParams
+			});
+			$httpBackend.flush();
+			expect($scope.readMe.html).toEqual(readMe.html);
+			$scope.editReadMe();
+			mockModalInstance.dismiss();
+			expect($scope.readMe.html).toEqual(readMe.html);
+		});
+
+		it("opens the new task modal and updates the number of tasks on confirm", function() {
+			$httpBackend.expectGET('/api/projects/1').respond(200, project);
+			$httpBackend.expectGET('/api/projects/1/readme').respond(404, { }, { }, 'Not Found');
+			var controller = $controller('projectController', {
+				$scope : $scope,
+				$routeParams: $routeParams
+			});
+			$httpBackend.flush();
+			expect($scope.project.numberOfOpenTasks).toEqual(project.numberOfOpenTasks);
+			$scope.newTask();
+			mockModalInstance.close();
+			expect($scope.project.numberOfOpenTasks).toEqual(project.numberOfOpenTasks + 1);
+			$scope.newTask();
+			mockModalInstance.close();
+			expect($scope.project.numberOfOpenTasks).toEqual(project.numberOfOpenTasks + 2);
+		});
+
+		it("opens the new task modal and leaves the number of tasks unchanged on cancel", function() {
+			$httpBackend.expectGET('/api/projects/1').respond(200, project);
+			$httpBackend.expectGET('/api/projects/1/readme').respond(404, { }, { }, 'Not Found');
+			var controller = $controller('projectController', {
+				$scope : $scope,
+				$routeParams: $routeParams
+			});
+			$httpBackend.flush();
+			expect($scope.project.numberOfOpenTasks).toEqual(project.numberOfOpenTasks);
+			$scope.newTask();
+			mockModalInstance.dismiss();
+			expect($scope.project.numberOfOpenTasks).toEqual(project.numberOfOpenTasks);
+		});
+
+		it("opens the delete project modal and redirects to the projects page on confirm", function() {
+			$httpBackend.expectGET('/api/projects/1').respond(200, project);
+			$httpBackend.expectGET('/api/projects/1/readme').respond(404, { }, { }, 'Not Found');
+			var controller = $controller('projectController', {
+				$scope : $scope,
+				$routeParams: $routeParams
+			});
+			$httpBackend.flush();
+			$scope.deleteProject();
+			mockModalInstance.close();
+			expect($location.path.calls.any()).toBe(true);
+			expect($location.path.calls.argsFor(0)).toEqual(['/projects']);
+		});
+
+		it("opens the delete project modal and does nothing on cancel", function() {
+			$httpBackend.expectGET('/api/projects/1').respond(200, project);
+			$httpBackend.expectGET('/api/projects/1/readme').respond(404, { }, { }, 'Not Found');
+			var controller = $controller('projectController', {
+				$scope : $scope,
+				$routeParams: $routeParams
+			});
+			$httpBackend.flush();
+			$scope.deleteProject();
+			mockModalInstance.dismiss();
+			expect($location.path.calls.any()).toBe(false);
+		});
+
+	});
+
+	describe("newProjectModalController", function() {
 
 		var $scope;
 
@@ -100,7 +273,7 @@ describe("myTasks", function() {
 				{'field': 'description', 'code': 'invalid', 'message': 'invalid description'}
 			];
 			$httpBackend.expectPOST('/api/projects/').respond(400, {'errors': errors});
-			var controller = $controller('new-project', {
+			var controller = $controller('newProjectModalController', {
 				$scope: $scope,
 				$uibModalInstance: undefined
 			});
@@ -113,7 +286,7 @@ describe("myTasks", function() {
 			var nameError = {'field': 'name', 'code': 'invalid', 'message': 'invalid name'};
 			var descriptionError = {'field': 'description', 'code': 'invalid', 'message': 'invalid description'}
 			$httpBackend.expectPOST('/api/projects/').respond(400, {'errors': [nameError]});
-			var controller = $controller('new-project', {
+			var controller = $controller('newProjectModalController', {
 				$scope: $scope,
 				$uibModalInstance: undefined
 			});
@@ -130,7 +303,7 @@ describe("myTasks", function() {
 			var createdProject;
 			var newProject = {"id": 4, "name": "My new project", "description": "This is a new project.", "numberOfOpenTasks": 0, "href": "/api/projects/4"};
 			$httpBackend.expectPOST('/api/projects/').respond(201, newProject);
-			var controller = $controller('new-project', {
+			var controller = $controller('newProjectModalController', {
 				$scope: $scope,
 				$uibModalInstance: {
 					close: function(data) {
