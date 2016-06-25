87792dd6e64f80a0efea4e3361fc7a68cb30f32a
Chris Rimes <chris.rimes@gmail.com>
2016-06-25 23:20:57 +0100

    Read the context path from the <base> element and use it in API calls.  This allows the 'server.contextPath' property to be overridden to deploy at a path other than the server root.

--- src/main/resources/static/js/services.js
+++ src/main/resources/static/js/services.js
@@ -1,23 +1,25 @@
 var myTasksServices = angular.module('myTasksServices', ['ngResource'])
 
-.factory('projectService', ['$resource', function($resource) {
-	return $resource('/api/projects/:id', {}, {
+.constant('contextPath', document.getElementsByTagName('base')[0].getAttribute('href'))
+
+.factory('projectService', ['$resource', 'contextPath', function($resource, contextPath) {
+	return $resource(contextPath + 'api/projects/:id', {}, {
 		list: {method: 'GET', isArray: true, transformResponse: [angular.fromJson, function(data) { return data.projects }]},
-		getReadMe:  {method: 'GET', url: '/api/projects/:id/readme'},
-		saveReadMe: {method: 'POST', url: '/api/projects/:id/readme'}
+		getReadMe:  {method: 'GET', url: contextPath + 'api/projects/:id/readme'},
+		saveReadMe: {method: 'POST', url: contextPath + 'api/projects/:id/readme'}
 	});
 }])
 
-.factory('taskService', ['$resource', function($resource) {
-	return $resource('/api/tasks/:id', {}, {
+.factory('taskService', ['$resource', 'contextPath', function($resource, contextPath) {
+	return $resource(contextPath + 'api/tasks/:id', {}, {
 		list: {method: 'GET', isArray: true, transformResponse: [angular.fromJson, function(data) { return data.tasks }]},
 		query: {method: 'GET', isArray: true, transformResponse: [angular.fromJson, function(data) { return data.tasks }]},
-		getReadMe:  {method: 'GET', url: '/api/tasks/:id/readme'}
+		getReadMe:  {method: 'GET', url: contextPath + 'api/tasks/:id/readme'}
 	});
 }])
 
-.factory('userService', ['$resource', function($resource) {
-	return $resource('/api/admin/users/:id', {}, {
+.factory('userService', ['$resource', 'contextPath', function($resource, contextPath) {
+	return $resource(contextPath + 'api/admin/users/:id', {}, {
 		list: {method: 'GET', isArray: true, transformResponse: [angular.fromJson, function(data) { return data.users }]},
 		getCurrent: {method: 'GET', params: {id: 'current'}}
 	});
