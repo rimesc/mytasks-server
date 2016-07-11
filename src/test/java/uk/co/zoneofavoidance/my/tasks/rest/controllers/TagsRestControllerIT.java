9f25e9411c010436a8e55ad985c14608902a9ae0
Chris Rimes <chris.rimes@gmail.com>
2016-07-11 23:09:03 +0100

    Use SQL scripts to set up separate data for each test suite, rather than using the sample data for all of them.

--- src/test/java/uk/co/zoneofavoidance/my/tasks/rest/controllers/TagsRestControllerIT.java
+++ src/test/java/uk/co/zoneofavoidance/my/tasks/rest/controllers/TagsRestControllerIT.java
@@ -2,14 +2,19 @@ package uk.co.zoneofavoidance.my.tasks.rest.controllers;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.Test;
+import org.springframework.test.context.jdbc.Sql;
 
 /**
  * Integration tests for {@link TagsRestController}.
  */
+@Sql(scripts = "/sql/setup_TagsRestControllerIT.sql", executionPhase = BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/cleanup.sql", executionPhase = AFTER_TEST_METHOD)
 @SuppressWarnings("checkstyle:magicnumber")
 public class TagsRestControllerIT extends RestControllerIT {
 
@@ -18,9 +23,9 @@ public class TagsRestControllerIT extends RestControllerIT {
       get("/api/tags/")
          .andExpect(status().isOk())
          .andExpect(jsonPath("tags", hasSize(3)))
-         .andExpect(jsonPath("tags[0]", equalTo("Bug")))
-         .andExpect(jsonPath("tags[1]", equalTo("Feature")))
-         .andExpect(jsonPath("tags[2]", equalTo("Version 1")));
+         .andExpect(jsonPath("tags[0]", equalTo("First")))
+         .andExpect(jsonPath("tags[1]", equalTo("Second")))
+         .andExpect(jsonPath("tags[2]", equalTo("Third")));
    }
 
    @Test
