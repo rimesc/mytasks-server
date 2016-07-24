f1b0e3aa16263d1619b6fd85304fc77cbc3d7bb6
Chris Rimes <chris.rimes@gmail.com>
2016-07-24 16:43:03 +0100

    Return the tags from the repository as a set so that we don't get duplicates when the same tag is used on more than one task.

--- src/test/java/uk/co/zoneofavoidance/my/tasks/repositories/TagRepositoryIT.java
+++ src/test/java/uk/co/zoneofavoidance/my/tasks/repositories/TagRepositoryIT.java
@@ -2,12 +2,13 @@ package uk.co.zoneofavoidance.my.tasks.repositories;
 
 import static java.util.stream.Collectors.toList;
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.function.Function;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import static org.junit.Assert.assertThat;
@@ -28,36 +30,26 @@ import uk.co.zoneofavoidance.my.tasks.domain.Tag;
  */
 @IntegrationTest
 @SpringApplicationConfiguration
-@ActiveProfiles("dev")
+@ActiveProfiles("test")
 @RunWith(SpringJUnit4ClassRunner.class)
+@Sql(scripts = "/sql/setup_TagRepositoryIT.sql", executionPhase = BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/cleanup.sql", executionPhase = AFTER_TEST_METHOD)
 public class TagRepositoryIT {
 
    @Autowired
    private TagRepository tags;
 
-   private Tag unusedTag;
-
-   @Before
-   public void setUp() {
-      unusedTag = tags.save(Tag.create("Unused"));
-   }
-
-   @After
-   public void tearDown() {
-      tags.delete(unusedTag);
-   }
-
    @Test
    public void findAllIncludesUnused() {
-      assertThat(getTagNames(TagRepository::findAll), containsInAnyOrder("Bug", "Feature", "Unused", "Version 1"));
+      assertThat(getTagNames(TagRepository::findAll), containsInAnyOrder("First", "Second", "Third", "Fourth", "Unused"));
    }
 
    @Test
    public void findUsedExcludesUnused() {
-      assertThat(getTagNames(TagRepository::findUsed), containsInAnyOrder("Bug", "Feature", "Version 1"));
+      assertThat(getTagNames(TagRepository::findUsed), containsInAnyOrder("First", "Second", "Third", "Fourth"));
    }
 
-   private List<String> getTagNames(final Function<TagRepository, List<Tag>> accessor) {
+   private List<String> getTagNames(final Function<TagRepository, Collection<Tag>> accessor) {
       return accessor.apply(tags).stream().map(Tag::getName).collect(toList());
    }
 
