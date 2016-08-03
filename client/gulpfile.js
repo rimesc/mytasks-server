72cfc10ea186ee4a2f5719418f68dae37543e3a0
Chris Rimes <chris.rimes@gmail.com>
2016-08-03 21:06:50 +0100

    Move static resources to the usual place (src/main/resources/static) and compile the less directly to target/classes/static, as relying on maven-resources-plugin doesn't work so well in Eclipse.
    Configure the project to minimize the number of spurious errors reported by Eclipse as it's apparently not possible to disable validation for 3rd-party libraries.

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,69 +1,26 @@
-var path = require('path'),
-    gulp = require('gulp'),
+var gulp = require('gulp'),
     less = require('gulp-less'),
     cssMinify = require('gulp-minify-css');
 
-var paths = {
-    baseUrl: 'file:' + process.cwd() + '/src/',
-    libs: ['bower_components/**'],
-    css: [],
-    less: ['src/less/*'],
-    fonts: ['bower_components/bootstrap/fonts/*'],
-    images: ["src/images/*"],
-    partials: ["src/partials/*"],
-    modals: ["src/modals/*"],
-    components: ["src/components/*"],
-    destination: './dist'
-};
+var bower = 'bower_components',
+    resources = 'src/main/resources/static',
+    target = 'target/classes/static';
 
-gulp.task('copy-css', function() {
-    return gulp.src(paths.css)
-        .pipe(gulp.dest(paths.destination + '/css'));
-});
-
-gulp.task('copy-js', function() {
-    return gulp.src('src/js/*.js')
-        .pipe(gulp.dest(paths.destination + '/js'));
+gulp.task('less', function () {
+    return gulp.src([resources + '/less/**'])
+        .pipe(less())
+        .pipe(cssMinify({noRebase: true}))
+        .pipe(gulp.dest(target + '/css'));
 });
 
 gulp.task('copy-lib', function() {
-    return gulp.src(paths.libs)
-        .pipe(gulp.dest(paths.destination + '/lib'));
-});
-
-gulp.task('copy-images', function() {
-    return gulp.src(paths.images)
-        .pipe(gulp.dest(paths.destination + '/images'));
-});
-
-gulp.task('copy-partials', function() {
-    return gulp.src(paths.partials)
-        .pipe(gulp.dest(paths.destination + '/partials'))
-});
-
-gulp.task('copy-modals', function() {
-    return gulp.src(paths.modals)
-        .pipe(gulp.dest(paths.destination + '/modals'))
-});
-
-gulp.task('copy-components', function() {
-    return gulp.src(paths.components)
-        .pipe(gulp.dest(paths.destination + '/components'))
-});
-
-gulp.task('less', function () {
-    return gulp.src(paths.less)
-        .pipe(less())
-        .pipe(cssMinify({noRebase: true}))
-        .pipe(gulp.dest(paths.destination + '/css'));
+	return gulp.src([bower + '/**']).pipe(gulp.dest(target + '/lib'));
 });
 
 // copies fonts from bootstrap to where they can be referenced by the compiled CSS
 gulp.task('copy-fonts', function() {
-    return gulp.src(paths.fonts)
-        .pipe(gulp.dest(paths.destination + '/fonts'));
+    return gulp.src([bower + '/bootstrap/fonts/**']).pipe(gulp.dest(target + '/fonts'));
 });
 
-gulp.task('default', ['copy-css', 'copy-js', 'copy-lib',
-    'copy-images', 'copy-partials', 'copy-modals', 'copy-components', 'less', 'copy-fonts'], function(){});
+gulp.task('default', ['less', 'copy-lib', 'copy-fonts'], function(){});
 
