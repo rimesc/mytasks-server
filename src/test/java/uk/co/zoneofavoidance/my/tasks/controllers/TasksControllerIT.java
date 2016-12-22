95eb527977cbe016d460a4a95865a17a35a4bcf8
Chris Rimes <chris.rimes@gmail.com>
2016-12-22 23:41:47 +0000

    Add missing integration tests for tasks controller.

--- src/test/java/uk/co/zoneofavoidance/my/tasks/controllers/TasksControllerIT.java
+++ src/test/java/uk/co/zoneofavoidance/my/tasks/controllers/TasksControllerIT.java
@@ -121,6 +121,79 @@ public class TasksControllerIT extends ControllerIT {
    }
 
    @Test
+   public void getTaskReturnsRequestedTask() throws Exception {
+      get("/api/tasks/1")
+         .andExpect(status().isOk())
+         .andExpect(content().json("{"
+            + "  'id': 1,"
+            + "  'summary': 'First task',"
+            + "  'priority': 'NORMAL',"
+            + "  'state': 'TO_DO',"
+            + "  'created': '2016-07-10T00:29:08.000+0000',"
+            + "  'tags': [ 'First', 'Second' ],"
+            + "  'notes': { 'html': '<p>My first task</p>', 'raw': 'My first task' },"
+            + "  'project': { 'id': 1, 'name': 'First project' },"
+            + "  'href': '/api/tasks/1'"
+            + "}", STRICT));
+      get("/api/tasks/2")
+         .andExpect(status().isOk())
+         .andExpect(content().json("{"
+            + "  'id': 2,"
+            + "  'summary': 'Second task',"
+            + "  'priority': 'LOW',"
+            + "  'state': 'TO_DO',"
+            + "  'created': '2016-07-10T00:29:08.000+0000',"
+            + "  'tags': [ ],"
+            + "  'notes': { 'html': '<p>My second task</p>', 'raw': 'My second task' },"
+            + "  'project': { 'id': 2, 'name': 'Second project' },"
+            + "  'href': '/api/tasks/2'"
+            + "}", STRICT));
+   }
+
+   @Test
+   public void getUnknownTaskIsNotFound() throws Exception {
+      get("/api/tasks/8")
+         .andExpect(status().isNotFound())
+         .andExpect(jsonPath("$.code", equalTo("Not Found")))
+         .andExpect(jsonPath("$.message", equalTo("The requested task could not be found.")));
+   }
+
+   @Test
+   public void getInvalidTaskIsBadRequest() throws Exception {
+      get("/api/tasks/abc")
+         .andExpect(status().isBadRequest())
+         .andExpect(jsonPath("$.code", equalTo("Bad Request")))
+         .andExpect(jsonPath("$.message", equalTo("Invalid task ID: abc")));
+   }
+
+   @Test
+   public void getTaskNotesReturnsNotesForRequestedTask() throws Exception {
+      get("/api/tasks/1/notes")
+         .andExpect(status().isOk())
+         .andExpect(content().json("{"
+            + "  'raw': 'My first task',"
+            + "  'html': '<p>My first task</p>',"
+            + "  'href': '/api/tasks/1/notes'"
+            + "}", STRICT));
+   }
+
+   @Test
+   public void getUnknownTaskNotesIsNotFound() throws Exception {
+      get("/api/tasks/8/notes")
+         .andExpect(status().isNotFound())
+         .andExpect(jsonPath("$.code", equalTo("Not Found")))
+         .andExpect(jsonPath("$.message", equalTo("The requested task could not be found.")));
+   }
+
+   @Test
+   public void getInvalidTaskNotesIsBadRequest() throws Exception {
+      get("/api/tasks/abc/notes")
+         .andExpect(status().isBadRequest())
+         .andExpect(jsonPath("$.code", equalTo("Bad Request")))
+         .andExpect(jsonPath("$.message", equalTo("Invalid task ID: abc")));
+   }
+
+   @Test
    @DirtiesContext
    public void postUpdateTaskSavesTaskIfValid() throws Exception {
       final String task = "{'summary': 'My edited task', 'description': 'This is an *edited* task.', 'priority': 'LOW', 'tags': ['First', 'Second']}";
@@ -153,93 +226,8 @@ public class TasksControllerIT extends ControllerIT {
       post("/api/tasks/1", task)
          .andExpect(status().isOk())
          .andExpect(jsonPath("$.id", equalTo(1)))
-         .andExpect(jsonPath("$.state", equalTo("IN_PROGRESS")));
+         .andExpect(jsonPath("$.state", equalTo("IN_PROGRESS")))
+         .andExpect(jsonPath("$.updated", dateWithin(1, SECONDS, new Date())));
    }
-
-   // @Test
-   // public void postNewProjectRaisesErrorForEmptyName() throws Exception {
-   // final String project = "{'name': '', 'description': 'This is a new
-   // project.'}";
-   // post("/api/projects/", project)
-   // .andExpect(status().isBadRequest())
-   // .andExpect(jsonPath("errors", hasSize(2)))
-   // .andExpect(jsonPath("errors[*].field", containsInAnyOrder("name",
-   // "name")))
-   // .andExpect(jsonPath("errors[*].code", containsInAnyOrder("Length",
-   // "NotEmpty")))
-   // .andExpect(jsonPath("errors[*].message", containsInAnyOrder("length must
-   // be between 1 and 255", "may not be empty")));
-   // }
-   //
-   // @Test
-   // public void getProjectReturnsRequestedProject() throws Exception {
-   // get("/api/projects/1")
-   // .andExpect(status().isOk())
-   // .andExpect(jsonPath("id", equalTo(1)))
-   // .andExpect(jsonPath("name", equalTo("My first project")))
-   // .andExpect(jsonPath("description", startsWith("This is my first sample
-   // project.")))
-   // .andExpect(jsonPath("numberOfOpenTasks", equalTo(2)))
-   // .andExpect(jsonPath("href", equalTo("/api/projects/1")));
-   // get("/api/projects/2")
-   // .andExpect(status().isOk())
-   // .andExpect(jsonPath("id", equalTo(2)))
-   // .andExpect(jsonPath("name", equalTo("My second project")))
-   // .andExpect(jsonPath("description", startsWith("This is my second sample
-   // project.")))
-   // .andExpect(jsonPath("numberOfOpenTasks", equalTo(2)))
-   // .andExpect(jsonPath("href", equalTo("/api/projects/2")));
-   // }
-   //
-   // @Test
-   // public void getUnknownProjectIsNotFound() throws Exception {
-   // get("/api/projects/6")
-   // .andExpect(status().isNotFound())
-   // .andExpect(jsonPath("code", equalTo("Not Found")))
-   // .andExpect(jsonPath("message", equalTo("The requested project could not be
-   // found.")));
-   // }
-   //
-   // @Test
-   // public void getInvalidProjectIsBadRequest() throws Exception {
-   // get("/api/projects/abc")
-   // .andExpect(status().isBadRequest())
-   // .andExpect(jsonPath("code", equalTo("Bad Request")))
-   // .andExpect(jsonPath("message", equalTo("Invalid project ID: abc")));
-   // }
-   //
-   // @Test
-   // public void getProjectReadMeReturnsReadMeIfAvailable() throws Exception {
-   // get("/api/projects/1/readme")
-   // .andExpect(status().isOk())
-   // .andExpect(jsonPath("markdown", startsWith("# Lorem ipsum")))
-   // .andExpect(jsonPath("html", startsWith("<h1>Lorem ipsum</h1>")));
-   // }
-   //
-   // @Test
-   // public void getUnknownProjectReadMeIsNotFound() throws Exception {
-   // get("/api/projects/6/readme")
-   // .andExpect(status().isNotFound())
-   // .andExpect(jsonPath("code", equalTo("Not Found")))
-   // .andExpect(jsonPath("message", equalTo("The requested project could not be
-   // found.")));
-   // }
-   //
-   // @Test
-   // public void getProjectReadMeIsNotFoundIfUnavailable() throws Exception {
-   // get("/api/projects/2/readme")
-   // .andExpect(status().isNotFound())
-   // .andExpect(jsonPath("code", equalTo("Not Found")))
-   // .andExpect(jsonPath("message", equalTo("The requested note could not be
-   // found.")));
-   // }
-   //
-   // @Test
-   // public void getInvalidProjectReadMeIsBadRequest() throws Exception {
-   // get("/api/projects/abc/readme")
-   // .andExpect(status().isBadRequest())
-   // .andExpect(jsonPath("code", equalTo("Bad Request")))
-   // .andExpect(jsonPath("message", equalTo("Invalid project ID: abc")));
-   // }
 
 }
